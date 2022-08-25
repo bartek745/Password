4ef8f49ad6785a8bcc2e777719bae3bcf193bbc3
bartek745 <65806045+bartek745@users.noreply.github.com>
2022-08-25 16:49:53 +0200

    Initial commit

---
+++ src/main/java/Password.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Password {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int password= scanner.nextInt();
+        if(password<=999&&password>=100){
+            for(int i=0;i<password;i++){
+if (password == i){
+            System.out.println(password);
+        }}
+    }else{
+            password=scanner.nextInt();
+            return;
+        }
+
+    }}
